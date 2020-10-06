5e47a8d48058be3e196ead5d965f62b9cd50f5a7
tom1k <tomascosta@ua.pt>
2020-10-06 19:15:22 +0100

    added help for task2 r3

--- Aula01/task2.py
+++ Aula01/task2.py
@@ -1,26 +1,29 @@
-def func1(n):
+def R1(n):
     if (n==0):
         return 0
-    return 1 + func1(n-1)
+    return 1 + R1(n-1)
 
-def func2(n):
+def R2(n):
     if (n==0):
         return 0
     if (n==1):
         return 1
-    return n + func2(n-2)
+    return n + R2(n-2)
 
-def func3(n):
+def R3(n):
     if (n==0):
         return 0
-    return 1 + 2 * func3(n-1)
+    return 1 + 2 * R3(n-1)
 
-def func4(n):
+def R4(n):
     if (n==0):
         return 0
-    return 1 + func4(n-1) * func4(n-1)
+    return 1 + R4(n-1) + R4(n-1)
 
-print("Func1: " , format(func1(3)))
-print("Func2: " , format(func2(3)))
-print("Func3: " , format(func3(3)))
-print("Func4: " , format(func4(3)))
+"""
+print("R1: " , format(R1(3)))
+print("R2: " , format(R2(3)))
+print("R4: " , format(R4(3)))
+"""
+for i in range(1,5):
+    print("R3(%d): %d" % (i,R3(i)))
