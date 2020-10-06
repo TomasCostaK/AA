f2400f8d43cde1758ef0c3ae4958e720aeced8f8
tom1k <tomascosta@ua.pt>
2020-10-06 19:59:26 +0100

    Added assertions for Task2 exercises

--- Aula01/task2.py
+++ Aula01/task2.py
@@ -20,10 +20,24 @@ def R4(n):
         return 0
     return 1 + R4(n-1) + R4(n-1)
 
+n = 12
+
+print("Return of R1 correct? " , format( R1(n)==n ))
+print("Return of R2 correct? " , format( R2(n)==sum(i if n % 2 == 0 else i+1 for i in range(0,n+1,2)) ))
+print("Return of R3 correct? " , format( R3(n)==(2**n -1) ))
+print("Return of R4 correct? " , format( R4(n)==(2**n -1) ))
+
 """
-print("R1: " , format(R1(3)))
-print("R2: " , format(R2(3)))
-print("R4: " , format(R4(3)))
-"""
-for i in range(1,5):
+for i in range(1,10):
     print("R3(%d): %d" % (i,R3(i)))
+    Retorno final: 2^n - 1
+
+# Ajuda para resolver a formula matematica, basicamente fazemos uma lista comprehension
+# para criar uma lista ou de impares ou de pares e depois fazemos o sum dessa lista
+
+return_val = R2(n)
+expected = sum(i if n % 2 == 0 else i+1 for i in range(0,n+1,2))
+
+print(return_val)
+print(expected)
+"""
