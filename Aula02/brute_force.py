d1234411b3d48473fbe67be9a617801125e17cf6
Tom1k <tomascosta@ua.pt>
2020-10-20 10:01:36 +0100

    Added final sols for div_n_conq and dec_n_conq

--- Aula02/brute_force.py
+++ Aula02/brute_force.py
@@ -13,10 +13,23 @@ def bf2(a,b):
     return a * bf2(a, b-1)
     
 
-def dnc(a,b):
+def div_n_conq(a,b):
     if b==0:
         return 1
-    return dnc(a,b/2) * dnc(a, (b+1)/2 )
+    if b==1:
+        return a
+    return div_n_conq(a,b//2) * div_n_conq(a, (b+1)//2 )
+
+def dec_n_conq(a,b):
+    if b==0:
+        return 1
+
+    sol = dec_n_conq(a, b//2)
+    
+    if sol%2:
+        return sol * sol;
+
+    return a * sol * sol
     
 
 a = 234115
@@ -26,7 +39,7 @@ tic = time.time()
 bf1(a,b)
 toc = time.time()
 
-print("Elapsed time for BF1: %.4fs" % (toc-tic))
+print("Elapsed time for BF1: %.6fs" % (toc-tic))
 
 # BF2 Attempt
 
@@ -34,12 +47,20 @@ tic = time.time()
 bf2(a,b)
 toc = time.time()
 
-print("Elapsed time for BF2: %.4fs" % (toc-tic))
+print("Elapsed time for BF2: %.6fs" % (toc-tic))
 
 # BF2 Attempt
 
 tic = time.time()
-dnc(a,b)
+div_n_conq(a,b)
 toc = time.time()
 
-print("Elapsed time for dnc: %.4fs" % (toc-tic))
+print("Elapsed time for div_n_conq: %.6fs" % (toc-tic))
+
+#Dec_n_conquer
+
+tic = time.time()
+dec_n_conq(a,b)
+toc = time.time()
+
+print("Elapsed time for dec_n_conq: %.6fs" % (toc-tic))
