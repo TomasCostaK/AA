fb9801d0a1538f532e525e418d488182b54f561c
Tom1k <tomascosta@ua.pt>
2020-10-20 10:22:07 +0100

    Small change to BF1 and added asserts

--- Aula02/brute_force.py
+++ Aula02/brute_force.py
@@ -1,19 +1,31 @@
 import time
 
 def bf1(a,b):
-    val = a
-    for i in range(1,b):
+
+    assert(type(b) == int) and (b>=0), "Wrong exponent!"
+    assert(a != 0 or b != 0), "Cannot compute 0**0"
+
+    val = 1
+    for i in range(1,b+1):
         val *= a
     return val
 
 
 def bf2(a,b):
+
+    assert(type(b) == int) and (b>=0), "Wrong exponent!"
+    assert(a != 0 or b != 0), "Cannot compute 0**0"
+
     if b==0:
         return 1
     return a * bf2(a, b-1)
     
 
 def div_n_conq(a,b):
+
+    assert(type(b) == int) and (b>=0), "Wrong exponent!"
+    assert(a != 0 or b != 0), "Cannot compute 0**0"
+
     if b==0:
         return 1
     if b==1:
@@ -21,6 +33,10 @@ def div_n_conq(a,b):
     return div_n_conq(a,b//2) * div_n_conq(a, (b+1)//2 )
 
 def dec_n_conq(a,b):
+
+    assert(type(b) == int) and (b>=0), "Wrong exponent!"
+    assert(a != 0 or b != 0), "Cannot compute 0**0"
+    
     if b==0:
         return 1
 
